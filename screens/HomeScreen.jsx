c7c07c30d5d4496ecb328e9f7083e76c35a8430e
Your Name <you@example.com>
2023-12-02 23:53:28 +0300

    signout

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
+import { auth } from '../firebase'
+
 
 export default function HomeScreen() {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>Geçerli Kullanıcı Email</Text>
+      <Text>{auth.currentUser?.email}</Text>
+      <TouchableOpacity onPress={handleSignOut} style={styles.buttonL}>
+          <Text style={styles.buttonText}>Çıkış Yap</Text>
+        </TouchableOpacity>
     </View>
   )
 }

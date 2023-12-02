54b5de86e0a70e330b9fe51d7ebf78c8a9467765
Your Name <you@example.com>
2023-12-03 00:26:17 +0300

    finished

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,18 +1,48 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { auth } from '../firebase'
+import { useNavigation } from '@react-navigation/native';
+
 
 
 export default function HomeScreen() {
+
+  const navigation = useNavigation();
+
+  const handleSignOut = () =>{
+    auth.signOut().then(()=>{
+      navigation.navigate("Login")
+    }).catch(error =>alert(error.message))
+  }
   return (
-    <View>
+    <View style={styles.container} >
       <Text>Geçerli Kullanıcı Email</Text>
       <Text>{auth.currentUser?.email}</Text>
-      <TouchableOpacity onPress={handleSignOut} style={styles.buttonL}>
-          <Text style={styles.buttonText}>Çıkış Yap</Text>
+      <TouchableOpacity  onPress={handleSignOut} style={styles.button}>
+          <Text style={styles.buttonText} >Çıkış Yap</Text>
         </TouchableOpacity>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container:{
+flex:1,
+justifyContent:"center",
+alignItems:"center"
+
+  },
+  button:{
+    backgroundColor: "#0782f9",
+    padding: 15,
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop:40,
+    width:"60%"
+  },
+  buttonText:{
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  }
+})
